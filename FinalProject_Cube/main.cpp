24af4b3d5f8a34930565e2a0416e61554431b5cf
Hyunwoo Seo <shw4008@gmail.com>
2024-05-28 15:17:21 +0900

    Initial Commit

---
+++ FinalProject_Cube/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  FinalProject_Cube
+//
+//  Created by 서현우 on 5/28/24.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
